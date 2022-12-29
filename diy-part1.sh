c6fa969df7f7181c418b8348753b68dc12be8bd6
OpenWrtLi <107589430+OpenWrtLi@users.noreply.github.com>
2022-12-29 17:31:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,45 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+rm -rf feeds/kenzo/luci-app-adguardhome
+rm -rf feeds/kenzo/adguardhome
+rm -rf feeds/packages/net/adguardhome
+rm -rf feeds/kenzo/luci-app-godproxy
+rm -rf feeds/kenzo/luci-app-ikoolproxy
+rm -rf feeds/kenzo/luci-app-tencentddns
+rm -rf feeds/kenzo/diy/.packages/luci-app-adguardhome
+rm -rf feeds/kenzo/diy/.packages/adguardhome
+rm -rf feeds/kenzo/luci-theme-neobird
+rm -rf feeds/kenzo/luci-theme-edge
+rm -rf feeds/kenzo/luci-app-fileassistant
+rm -rf feeds/luci/applications/luci-app-v2ray-server
+rm -rf feeds/luci/applications/luci-app-socat
+rm -rf feeds/kenzo/luci-app-upnp
+rm -rf feeds/kenzo/luci-app-advanced
+rm -rf feeds/luci/applications/luci-app-aliyundrive-fuse
+rm -rf feeds/luci/applications/luci-app-aliyundrive-webdav
+rm -rf feeds/luci/applications/luci-app-amule
+rm -rf feeds/luci/applications/luci-app-diskman
+rm -rf feeds/kenzo/luci-app-openvpn-server
+rm -rf feeds/kenzo/luci-app-softethervpn
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/themes/luci-theme-argon-mod
+rm -rf feeds/luci/applications/luci-app-baidupcs-web
+rm -rf feeds/kenzo/luci-app-wrtbwmon
+rm -rf feeds/kenzo/luci-app-xlnetacc
+rm -rf feeds/kenzo/luci-app-tencentddns
+rm -rf feeds/kenzo/luci-app-jd-dailybonus
+rm -rf package/lean/autosamba
+rm -rf feeds/luci/applications/luci-app-v2ray-server
+rm -rf feeds/luci/applications/luci-app-netdata
+rm -rf feeds/luci/applications/luci-app-samba4
+rm -rf feeds/luci/applications/luci-app-samba
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/kenzo/luci-theme-argon
+rm -rf package/lean/luci-theme-argon
+rm -rf package/lean/luci-theme-edge
+rm -rf package/lean/luci-theme-neobird
+rm -rf package/lean/luci-theme-opentopd
 git clone https://github.com/bootli/libuild package/libuild
 git clone https://github.com/bootli/luci-app-v2ray-server.git feeds/luci/applications/luci-app-v2ray-serve
 git clone https://github.com/messense/aliyundrive-fuse.git package/li/aliyundrive-fuse
